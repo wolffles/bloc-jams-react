5ac6e748cc0b1e1797f3020df226567056cd7637
woffles <wolfgang.truong@gmail.com>
2018-08-22 12:05:39 -0700

    checkpoint complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ class Album extends Component {
     this.state =  {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false,
       isHovered: false
     };
@@ -21,6 +23,25 @@ class Album extends Component {
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  componentDidMOunt() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate',this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+     this.audioElement.src = null;
+     this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+     this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+   }
 
   play() {
     this.audioElement.play();
@@ -61,6 +82,12 @@ class Album extends Component {
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
+  }
+
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime:newTime });
   }
 
   clickToggle(song) {
@@ -110,7 +137,7 @@ class Album extends Component {
                   {this.state.isHovered === idx + 1 ? (
                      <span><Ionicon icon={this.hoverToggle(ele)} /></span>
                    ) : (
-                   this.state.isPlaying && this.state.currentSong === ele || !this.state.isPlaying && this.state.currentSong === ele ?
+                  (this.state.isPlaying && this.state.currentSong === ele )|| (!this.state.isPlaying && this.state.currentSong === ele) ?
                      <span className={this.clickToggle(ele)} ></span>
                      : (idx + 1)
                    )}
@@ -127,9 +154,12 @@ class Album extends Component {
         <PlayerBar
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
+          currentTime={this.audioElement.currentTime}
+          duration={this.audioElement.duration}
           handleSongClick={()=> this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={()=> this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
         />
       </section>
     );
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -16,9 +16,16 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">-:--</div>
-          <input type="range" className="seek-bar" value="0"/>
-          <div className="total-time">-:--</div>
+          <div className="current-time">{this.props.currentTime}</div>
+          <input
+            type="range"
+            className="seek-bar"
+            onChange={this.props.handleTimeChange}
+            value={(this.props.currentTime / this.props.duration) || 0}
+            max = "1"
+            min = "0"
+            step = "0.01"
+          />
         </section>
         <section id="volume-control">
           <div className="icon ion-volume-low"></div>
